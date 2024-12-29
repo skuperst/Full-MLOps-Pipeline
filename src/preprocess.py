0200bcad14bb85a133f65a5da1983d0c59010dc8
Stanislav <stas@aytm.com>
2024-12-29 23:00:38 +0100

    Split the data into current and future

--- src/preprocess.py
+++ src/preprocess.py
@@ -29,14 +29,6 @@ def preprocess(**kwargs):
     Data = pd.read_csv(os.path.join(curr_dir, os.pardir, kwargs['input_folder'], kwargs['input_file']))
     logging.info("The data has {} rows and {} columns".format(*Data.shape))
 
-    # Rename the columns
-    try:
-        Data.rename(columns=kwargs['rename_map'], inplace=True)
-        logging.info("Columns renamed.")
-    except:
-        logging.error("Name mismatch in the column renaming dictionary!")
-        sys.exit(1)
-
     # Remove all rows with NaNs (if any)
     row_has_NaN = Data.isnull().any(axis=1)
     total_NaNs = row_has_NaN.sum()
@@ -46,12 +38,31 @@ def preprocess(**kwargs):
     else:
         logging.info("No missing data points were found.")
 
-
     # Drop the duplicates (or not)
     if kwargs['keep_duplicates'] == False:
         Data = Data[~Data.duplicated(list(Data.columns))]
         logging.info("Duplicates removed (if any).")
 
+    # Rename the columns
+    try:
+        Data.rename(columns=kwargs['rename_map'], inplace=True)
+        logging.info("Columns renamed.")
+    except:
+        logging.error("Name mismatch in the column renaming dictionary!")
+        sys.exit(1)
+
+    # Split the file into the 'current' and 'future' parts. This will be used by EvidentlyAI later on
+    dataset_size = Data.shape[0]
+    # Probabilities used to split the data set to the 'current' and the 'future' subsets
+    # For prob_coeff=0 the probabilities to fall into either dataset are the same
+    probabilities = np.exp(kwargs['prob_coeff'] * ((np.arange(dataset_size, dtype=float) / (dataset_size - 1))))
+    # Normalization
+    probabilities /= probabilities.sum()
+    sampled_indices = np.random.choice(Data.sort_values(kwargs["column_impacting_the_split"], ascending=True).index, 
+                                       size=int(kwargs["future_subset_size"] * dataset_size), p=probabilities, replace=False)
+    # Add the split column values
+    Data[kwargs["time_period_column_name"]] = pd.Series(Data.index.isin(sampled_indices)).map({True: 'future', False: 'current'})
+    logging.info("Dataset split into the current and the future subsets, {} and {} in size respectively.".format(dataset_size, sampled_indices.size))
 
     # Start an MLflow run
     with mlflow.start_run():
@@ -59,51 +70,67 @@ def preprocess(**kwargs):
         mlflow.set_tag('mlflow.runName', "Preprocess: {}".format(datetime.now().strftime("%Y/%m/%d (%H:%M)")))
 
         # Add graphs for all of the features
-        for idx, col, t in Data.dtypes.reset_index().reset_index().values:
-            plt.figure(figsize=(8, 6))
+        for idx, col, t in Data.drop(kwargs["time_period_column_name"], axis=1).dtypes.reset_index().reset_index().values:
+            fig, axs = plt.subplots(2, 1, figsize=(12, 6))
             # All colors are from the same cmap
             color = plt.colormaps.get_cmap('Dark2')(idx / Data.columns.size)
-            if t == 'object':
-                plt.bar(*zip(*Data[col].value_counts().reset_index().values), color=color)
-                # Rotate the labels, if there are too many of them
-                if Data[col].nunique() > 5:
-                    plt.xticks(rotation = 60)
-            else:
-                plt.hist(Data[col], bins = 100, color=color)
-            plt.title(col, fontsize=16)
-            plt.ylabel('Count')
-            plt.tight_layout()
-            if set(Data[col]).issubset(set([0., 1.])):
-                # Remove all ticks and labels
-                plt.xticks([])  # Remove x-axis ticks
-                plt.yticks([])  # Remove y-axis ticks
-                plt.tick_params(bottom=False, left=False)  # Remove tick marks
-                # Add custom ticks for 0 and 1
-                plt.xticks([0, 1], ['No', 'Yes'])
+            for i, tp in zip([0,1], ['current','future']):
 
-            mlflow.log_figure(plt.gcf(), "{}.png".format(col))
-        plt.close()
-        del(idx, col, t)
+                x, y = zip(*Data[Data[kwargs['time_period_column_name']]==tp][col].value_counts().sort_index().reset_index().values)
+
+                # Calculate optimal bar width
+                total_space = 1.0  # Total space allocated for one bar + one gap
+                bar_width = max(0.009, total_space * 0.8 / len(x)) # Use 80% of space for bars
+
+                # Create the bar graph
+                axs[i].bar(x, y, color=color, align='center', edgecolor='black', width=bar_width)
+
+                # Adjust x-axis limits to ensure bars are fully visible
+                if t!='object':
+                    if tp=='current':
+                        x_lim_min = x[0] - bar_width / 2
+                        x_lim_max = x[-1] + bar_width / 2
+                    axs[i].set_xlim(x_lim_min, x_lim_max)
+
+                # Rotate the labels, if there are too many of them
+                if t=='object' and len(set(x)) > 5:
+                    axs[i].tick_params(axis='x', labelrotation=60)
+
+                # Add custom ticks for 0 and 1
+                if set(x).issubset(set([0., 1.])):
+                    axs[i].set_xticks([0, 1], ['No', 'Yes'])
+
+                axs[i].set_title("{} ({})".format(col, tp), fontsize=16)
+                axs[i].set_ylabel('Count')
+
+            plt.tight_layout()
+            mlflow.log_figure(fig, "{}.png".format(col))
+            plt.close(fig)
+        del(idx, col, t, color, i, tp, total_space, bar_width)
 
     logging.info("{} data histograms (one per column) were added to the MLFlow.".format(len(Data.columns)))
+    
 
     # One-hot transformation
-    categorical_columns =  Data.select_dtypes(exclude=['int', 'float']).columns
+    # All string columns, except the current/future column
+    categorical_columns =  Data.drop(kwargs["time_period_column_name"], axis=1).select_dtypes(exclude=['int', 'float']).columns
+    # All numerical columns
+    non_categorical_columns =  Data.select_dtypes(include=['int', 'float']).columns
     # Dictionary to keep track of the name changes after pd.get_dummies
     onehot_name_dictionary = dict()
     # Add the non-categorical columns to the dictionary
-    for col in Data.select_dtypes(include=['int', 'float']).columns:
+    for col in non_categorical_columns:
         onehot_name_dictionary[col] = col
     # Convert categorical data with the one-hot transform and add the categorical columns to the dictionary
     if len(categorical_columns) > 0:
-        Data = pd.get_dummies(Data, categorical_columns, drop_first=False)
+        Data = pd.get_dummies(Data, columns=categorical_columns, drop_first=False, prefix=None)
         for col_old in categorical_columns:
             for col_new in Data.columns:
                 if col_new.startswith(col_old):
                     onehot_name_dictionary[col_new] = col_old
-        logging.info('One-hot transform was applied for the following columns: {}.'.format(', '.join(categorical_columns)))
+        logging.info('One-hot encoding was applied for the following columns: {}.'.format(', '.join(categorical_columns)))
     else:
-        logging.info('No categorical columns detected. One-hot transform is not applied.')
+        logging.info('No categorical columns detected. One-hot encoding is not applied.')
 
     # Save the names dictionary which relates the columnns before and after pd.get_dummies
     try:
@@ -114,12 +141,22 @@ def preprocess(**kwargs):
         logging.error("The column names dictionary was not saved!")
         sys.exit(1)
 
-    # Save the preprocessed file
+    # Save the 'current' preprocessed file
     try:
-        Data.to_csv(os.path.join(curr_dir, os.pardir, kwargs['output_file_path']), index=False)
-        logging.info("The preprocessed CSV file was successfully saved.")
+        Data[Data[kwargs["time_period_column_name"]]=='current'].drop(kwargs["time_period_column_name"], axis=1).to_csv(os.path.join(curr_dir, 
+                                                                                            os.pardir, kwargs['output_current_data_file_path']), index=False)
+        logging.info("The preprocessed current data CSV file was successfully saved.")
     except:
-        logging.error("The preprocessed CSV file was not saved!")
+        logging.error("The preprocessed current data CSV file was not saved!")
+        sys.exit(1)
+
+    # Save the 'future' preprocessed file
+    try:
+        Data[Data[kwargs["time_period_column_name"]]=='future'].drop(kwargs["time_period_column_name"], axis=1).to_csv(os.path.join(curr_dir, 
+                                                                                            os.pardir, kwargs['output_future_data_file_path']), index=False)
+        logging.info("The preprocessed future data CSV file was successfully saved.")
+    except:
+        logging.error("The preprocessed future data CSV file was not saved!")
         sys.exit(1)
 
     del(Data)
