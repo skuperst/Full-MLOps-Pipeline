b5996e796c054e2fd494480e750bb14c8be03c6a
Stanislav <stanislav.kuperstein@gmail.com>
2025-03-25 04:21:33 +0100

    Added PyTest

---
+++ src/test_pipeline.py
@@ -0,0 +1,20 @@
+import shutil
+import subprocess
+import os, yaml
+
+def test_dvc_repro():
+
+    # Current directory
+    curr_dir = os.path.dirname(os.path.abspath(__file__))
+    # The folder parameters
+    params = yaml.safe_load(open(os.path.join(curr_dir, os.pardir, "params.yaml")))['download_current_data']
+    data_folder = params['data_folder']
+    raw_current_data_file = params['raw_current_data_file']
+
+    # Copy test file into the (fake) current data file
+    shutil.copy(os.path.join(curr_dir, os.pardir, data_folder, 'test_data.csv'), 
+                os.path.join(curr_dir, os.pardir, data_folder, raw_current_data_file))
+
+    result = subprocess.run(["dvc", "repro", "--force"], capture_output=True, text=True)
+
+    assert result.returncode == 0
